dba97463d1a7bc98380afb0cca2a9b88cbf97107
sarveshwarbansal <104439314+sarveshwarbansal@users.noreply.github.com>
2024-01-13 12:55:40 +0530

    Add files via upload

---
+++ src/app/page.tsx
@@ -0,0 +1,29 @@
+import Image from "next/image";
+import Navbar from "../components/Navbar";
+import YoutubeVideo from "../components/YoutubeVideo";
+import Broker from "../components/Broker";
+import Hero from "../components/Hero";
+import Features from "../components/Features";
+import Stats from "../components/Stats";
+import Footer from "../components/Footer";
+import Pricing from "../components/Pricing";
+import Testimonials from "../components/Testimonials";
+import Cta from "../components/Cta";
+
+
+export default function Home() {
+  return (
+    <div id="home">
+      <Navbar/>
+      <Hero />
+      <Stats />
+      <Broker />
+      <YoutubeVideo videoId="1oDhFnkiRRo" />
+      <Features />
+      <Pricing />
+      <Testimonials />
+      <Cta />
+      <Footer />
+    </div>
+  );
+}
